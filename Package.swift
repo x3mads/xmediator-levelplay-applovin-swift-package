6af077bc8e40dccb653b3484c3032045c699d5be
XMediator CI <vision-team@x3mads.com>
2026-09-23 20:49:31 +0000

    Release 5.7.1

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "XMediatorLevelPlayAppLovinAdapterCompatibility",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(name: "LevelPlayAppLovinAdapter", targets: ["LevelPlayAppLovinAdapterWrapper"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ironsource-mobile/LevelPlay-AppLovin-Adapter-Swift-Package.git", exact: "5.7.1"),
+    ],
+    targets: [
+        .target(
+            name: "LevelPlayAppLovinAdapterWrapper",
+            dependencies: [
+                .product(
+                    name: "AppLovinAdapter",
+                    package: "LevelPlay-AppLovin-Adapter-Swift-Package",
+                    moduleAliases: ["AppLovinAdapter": "LevelPlayAppLovinAdapterTarget"]
+                ),
+            ]
+        ),
+    ]
+)
